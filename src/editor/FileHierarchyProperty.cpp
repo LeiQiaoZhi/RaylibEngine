6fcdb8a422ebf813c3864226980fae0f943929aa
Qiaozhi Lei <georgeleiqiaozhi@gmail.com>
2025-02-09 00:46:09 -0800

    assets action buttons

--- src/editor/FileHierarchyProperty.cpp
+++ src/editor/FileHierarchyProperty.cpp
@@ -7,15 +7,27 @@ void FileHierarchyProperty::OnEditorGUI(Rectangle &rect) {
     // Title
     const char *title = GetFileName(path.c_str());
     const bool isFile = IsPathFile(path.c_str());
-    const int icon = isFile ? ICON_FILETYPE_TEXT : folded ? ICON_FOLDER : ICON_FOLDER_OPEN;
+    const int icon = isFile ? ICON_FILETYPE_TEXT : folded ? ICON_ARROW_RIGHT_FILL : ICON_ARROW_DOWN_FILL;
     title = GuiIconText(icon, title);
 
     // Title UI
-    GuiDummyRec({rect.x, rect.y, rect.width, Editor::TextSize() * 1.0f}, "");
-    if (GuiLabelButton({rect.x, rect.y, rect.width, Editor::TextSize() * 1.0f}, title)) {
+    Rectangle titleRect = {rect.x, rect.y, rect.width, Editor::TextSize() * 1.0f};
+    if (GuiLabelButton(titleRect, title)) {
         folded = !folded;
+    }
+    
+    // Hover -- show action buttons
+    if (CheckCollisionPointRec(GetMousePosition(), titleRect)) {
+        const float buttonWidth = Editor::TextSize() * 1.2f;
+        Rectangle endRect = {rect.x + rect.width - buttonWidth, rect.y, buttonWidth, Editor::TextSize() * 1.0f};
+        if (GuiLabelButton(endRect, GuiIconText(ICON_FOLDER_OPEN, ""))) {
+            Utils::OpenFileInFileExplorer(path);
+        }
+        endRect.x -= buttonWidth + Editor::SmallGap();
         if (isFile) {
-            Utils::OpenFileWithDefaultApp(path);
+            if (GuiLabelButton(endRect, GuiIconText(ICON_FILE_OPEN, ""))) {
+                Utils::OpenFileWithDefaultApp(path);
+            }
         }
     }
     rect.y += Editor::TextSize() + Editor::SmallGap();
--- src/utils/Utils.h
+++ src/utils/Utils.h
@@ -9,15 +9,28 @@
 
 class Utils {
 public:
-    static void OpenFileWithDefaultApp(const std::string& filepath) {
+    static void OpenFileWithDefaultApp(const std::string &filepath) {
 #ifdef _WIN32
         ShellExecuteA(NULL, "open", filepath.c_str(), NULL, NULL, SW_SHOWNORMAL);  // Windows
 #elif __APPLE__
         std::string command = "open " + filepath;
-        system(command.c_str());  // macOS
+        system(command.c_str()); // macOS
 #elif __linux__
         std::string command = "xdg-open " + filepath;
         system(command.c_str());  // Linux
+#endif
+    }
+
+    static void OpenFileInFileExplorer(const std::string &filepath) {
+#ifdef _WIN32
+        std::string command = "explorer /select,\"" + filepath + "\"";  // Select file in Explorer
+        system(command.c_str());
+#elif __APPLE__
+        std::string command = "open -R " + filepath; // Reveal file in Finder
+        system(command.c_str());
+#elif __linux__
+        std::string command = "xdg-open \"" + filepath.substr(0, filepath.find_last_of('/')) + "\"";  // Open folder
+        system(command.c_str());
 #endif
     }
 };
